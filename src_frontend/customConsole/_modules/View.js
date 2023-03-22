ac61111baf956255944f58d7a28c17651bc8c17a
Tomoya Koyanagi <tomk79@gmail.com>
2023-03-23 00:19:55 +0900

    CCE: 記事詳細画面を追加

--- src_frontend/customConsole/_modules/View.js
+++ src_frontend/customConsole/_modules/View.js
@@ -4,11 +4,14 @@
 module.exports = function(state, cceAgent){
 	const blogList = new (require('../_pages/BlogList.js'))(state, cceAgent);
 	const articleList = new (require('../_pages/ArticleList.js'))(state, cceAgent);
+	const article = new (require('../_pages/Article.js'))(state, cceAgent);
 
     this.refresh = function(){
         const page = state.getState('page');
         if( page == 'ArticleList' ){
             articleList.draw();
+        }else if( page == 'Article' ){
+            article.draw();
         }else{
             blogList.draw();
         }
